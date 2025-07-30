b66cc5868a181ef4c21d9bff9bf2adfd0a9b01a7
João Victor <joaovictorsalotialves12@gmail.com>
2025-07-30 14:46:33 -0300

    Atualizando atributos HTML com JavaScript

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function toggleMode() {
   const html = document.documentElement
-
   html.classList.toggle('light')
+
+  const img = document.querySelector('#profile img')
+
+  if(html.classList.contains('light')) {
+    img.setAttribute('src', './assets/avatar-light.png')
+    img.setAttribute('alt', 'Foto de Mayk Brito sorrindo, usando óculos de sol e jaqueta preta e fundo azul.')
+  } else {
+    img.setAttribute('src', './assets/avatar.png')
+    img.setAttribute('alt', 'Foto de Mayk Brito sorrindo, usando óculos e camisa preta, barba e fundo amarelo.')
+  }
 }
